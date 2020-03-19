faa35c023ba3a7dc0fabd52ff5cad2171aaab32a
Natanael <natanaelgplay@gmail.com>
2020-03-19 22:07:52 +0000

    Update build-appimage.sh

--- build-appimage.sh
+++ build-appimage.sh
@@ -3,7 +3,7 @@
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # Retry if 503 error occurs, is a ostre bug
 sudo flatpak install -v --noninteractive --system flathub $1.Locale || {
-  sudo flatpak install -v --noninteractive --system flathub $1.Locale
+  sudo flatpak install -v --noninteractive --system flathub $1.Locale || true
 }
 sudo flatpak install -v --noninteractive --system flathub $1
 sudo ./flatpak2appdir $1
