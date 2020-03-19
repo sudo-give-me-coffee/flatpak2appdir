b25a1e7aff489a32ce0da2258d845c119eabd178
probonopd <probonopd@users.noreply.github.com>
2020-03-19 18:12:16 +0000

    Update and rename build-gedit.sh to build-appimage.sh
    [ci skip]

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -v --noninteractive --system flathub $ID
-sudo ./flatpak2appdir $ID
+sudo flatpak install -v --noninteractive --system flathub $1
+sudo ./flatpak2appdir $1
 
 export VERSION=$(LANG=en flatpak info $ID | sed 's/^[[:space:]]*//'  | grep -i ^Version: | cut -c 10-)
 
