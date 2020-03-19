e895954accea56f7145057d1f8bd1a074710dd97
Natanael <natanaelgplay@gmail.com>
2020-03-19 14:12:12 +0000

    Update build-gedit.sh

--- build-gedit.sh
+++ build-gedit.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 export LANG=en
+sudo flatpak install -v --noninteractive --system flathub org.gnome.gedit.Locale
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install -v --noninteractive --system flathub org.gnome.gedit
-sudo flatpak remove -v --noninteractive --system org.gnome.gedit.Locale
-sudo flatpak install -v --noninteractive --system flathub org.gnome.gedit.Locale
 sudo ./flatpak2appdir org.gnome.gedit
 
 export VERSION=$(flatpak info org.gnome.gedit | sed 's/^[[:space:]]*//'  | grep -i ^Version: | cut -c 10-)
