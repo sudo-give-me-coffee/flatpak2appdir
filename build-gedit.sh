88d05d449ea648ae2f5b625188baa1d66bde927e
Natanael <natanaelgplay@gmail.com>
2020-03-19 17:33:46 +0000

    Update build-gedit.sh

--- build-gedit.sh
+++ build-gedit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export LANG=en
 # Retry if 503 error occurs, is a ostre bug
 sudo flatpak install -v --noninteractive --system flathub org.gnome.gedit.Locale || {
   sudo flatpak install -v --noninteractive --system flathub org.gnome.gedit.Locale
@@ -9,7 +8,7 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak install -v --noninteractive --system flathub org.gnome.gedit
 sudo ./flatpak2appdir org.gnome.gedit
 
-export VERSION=$(flatpak info org.gnome.gedit | sed 's/^[[:space:]]*//'  | grep -i ^Version: | cut -c 10-)
+export VERSION=$(LANG=en flatpak info org.gnome.gedit | sed 's/^[[:space:]]*//'  | grep -i ^Version: | cut -c 10-)
 
 ./appimagetool-*.AppImage ./*.AppDir
 rm ./appimagetool-*.AppImage
